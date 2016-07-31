dd98266b0882bfdd5a355f08da24b83015704bfa
Marko Cupic <m.cupic@gmx.ch>
2016-08-01 00:53:05 +0200

    dev

--- config/config.php
+++ config/config.php
@@ -6,9 +6,5 @@
  * Time: 12:33
  */
 
-// CSS
-$GLOBALS['TL_CSS'][] = 'system/modules/fixed_tabs_aside/assets/fixedTabsAside.css';
-
-
 // Content element
 $GLOBALS['TL_CTE']['fixedTabsAside']['fixedTabsAsideItem'] = 'MCupic\FixedTabsAsideItem';
